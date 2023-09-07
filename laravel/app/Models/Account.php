ea5816960550c87cb9fe42fbf2b26c7cab8eb526
Takeshi Ueda <uedatakeshi@gmail.com>
2023-09-07 19:02:14 +0900

    add controller and model

---
+++ laravel/app/Models/Account.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'email',
+        'password',
+        'auth',
+        'status',
+        'version',
+        'login_date',
+    ];
+
+}
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    'accounts' => AccountController::class,
+]);
